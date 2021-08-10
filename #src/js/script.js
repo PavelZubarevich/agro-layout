adbfa1cdadcb926ab5451a4eed01274117cb42f9
PavelZ <pavelzubarevich@gmail.com>
2021-08-10 23:40:13 +0300

    bug fix

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,49 +1,28 @@
-@@include('mySlider.js');
+let burger = document.querySelector(".burger"),
+	headerTop = document.querySelector(".header__top"),
+	headerNav = document.querySelector(".header-top__nav"),
+	headerBtn = document.querySelector(".header-top__btn"),
+	headerLogo = document.querySelector(".header-top__logo");
 
-
-
-let burger = document.querySelector('.burger'),
-    headerTop = document.querySelector('.header__top'),
-    headerNav = document.querySelector('.header-top__nav'),
-    headerBtn = document.querySelector('.header-top__btn'),
-    headerLogo = document.querySelector('.header-top__logo')
-    
-burger.addEventListener('click', () => {
-    burger.classList.toggle('_active')
-    headerTop.classList.toggle('_active')
-    headerNav.classList.toggle('_active')
-    headerBtn.classList.toggle('_active')
-    headerLogo.classList.toggle('_active')
-})
-
-
-
-
-
-let list = document.querySelector('.header-nav__list');
-let header = document.querySelector('.header-top__content');
-let width = window.innerWidth;
-if(window.innerWidth < 576) {
-    list.appendChild(headerBtn);
-} else {
-    window.addEventListener('resize', () => {
-        if(window.innerWidth < 576) {
-            list.appendChild(headerBtn);
-        } else {
-            header.appendChild(headerBtn);
-        }
-    })
-}
-
-
-const slider = new Slider({
-    dots: true,
-    infinity: true,
-    autoHeight: true,
-    
+burger.addEventListener("click", () => {
+	burger.classList.toggle("_active");
+	headerTop.classList.toggle("_active");
+	headerNav.classList.toggle("_active");
+	headerBtn.classList.toggle("_active");
+	headerLogo.classList.toggle("_active");
 });
 
-slider.init();
-
-
-
+let list = document.querySelector(".header-nav__list");
+let header = document.querySelector(".header-top__content");
+let width = window.innerWidth;
+if (window.innerWidth < 576) {
+	list.appendChild(headerBtn);
+} else {
+	window.addEventListener("resize", () => {
+		if (window.innerWidth < 576) {
+			list.appendChild(headerBtn);
+		} else {
+			header.appendChild(headerBtn);
+		}
+	});
+}
