c2ff003c6b254dc6d321d7a6ce7333af20f5ee52
Pavel <pavelzubarevich@gmail.com>
2022-10-22 17:35:28 +0300

    Updates

---
+++ js/script.js
@@ -0,0 +1,28 @@
+"use strict";
+
+var burger = document.querySelector(".burger"),
+  headerTop = document.querySelector(".header__top"),
+  headerNav = document.querySelector(".header-top__nav"),
+  headerBtn = document.querySelector(".header-top__btn"),
+  headerLogo = document.querySelector(".header-top__logo");
+burger.addEventListener("click", function () {
+  burger.classList.toggle("_active");
+  headerTop.classList.toggle("_active");
+  headerNav.classList.toggle("_active");
+  headerBtn.classList.toggle("_active");
+  headerLogo.classList.toggle("_active");
+});
+var list = document.querySelector(".header-nav__list");
+var header = document.querySelector(".header-top__content");
+var width = window.innerWidth;
+if (window.innerWidth < 576) {
+  list.appendChild(headerBtn);
+} else {
+  window.addEventListener("resize", function () {
+    if (window.innerWidth < 576) {
+      list.appendChild(headerBtn);
+    } else {
+      header.appendChild(headerBtn);
+    }
+  });
+}
